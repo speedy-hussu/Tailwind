68b0ba7cbe8480d8d0f33aa8dd00e534a06f9e63
speedy-hussu <speedyhussu.work@gmail.com>
2025-08-30 00:08:53 +0530

    footer font

--- src/components/FoLinks.jsx
+++ src/components/FoLinks.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function FoLinks({ title, l1, l2, l3 }) {
   return (
-    <div className="text-white flex flex-col text-center md:text-start">
-      <h1 className="text-lg text-blue-300">{title}</h1>
+    <div className=" text-white text-sm flex flex-col text-center md:text-start">
+      <h1 className="text-base text-blue-300">{title}</h1>
       <p>{l1}</p>
       <p>{l2}</p>
       <p>{l3}</p>
--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -3,22 +3,22 @@ import FoLinks from "../components/FoLinks";
 import speedy from "../assets/speedy.png";
 function Footer() {
   return (
-    <div className="w-full flex flex-col justify-evenly items-center bg-black py-10 md:flex-row gap-10">
+    <div className="w-full flex justify-evenly items-center bg-black py-10  gap-10">
       <img src={speedy} className="h-20 md:h-30 rounded-full" />
       <FoLinks
-        title={"Speedy Links"}
+        title={"Speedy Skills"}
         l1={"about us"}
         l2={"contact us"}
         l3={"Policy"}
       />
       <FoLinks
-        title={"Speedy Links"}
+        title={"Products"}
         l1={"about us"}
         l2={"contact us"}
         l3={"Policy"}
       />
       <FoLinks
-        title={"Speedy Links"}
+        title={"Links"}
         l1={"about us"}
         l2={"contact us"}
         l3={"Policy"}
