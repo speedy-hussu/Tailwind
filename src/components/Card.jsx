a99eb276a46fef30e10991d1ef02410ef7a1a772
speedy-hussu <speedyhussu.work@gmail.com>
2025-08-29 23:31:39 +0530

    font size

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,8 @@ function Card({ title, desc, img }) {
   return (
     <div className=" w-20 flex flex-col items-center text-white m-5 md:w-45">
       <img src={img} className="w-30 p-2 h-auto mb-2 bg-white rounded-full" />
-      <h1 className="font-bold text-xl">{title}</h1>
-      <p className="text-lg text-center text-gray-300">{desc}</p>
+      <h1 className="font-bold md:text-xl">{title}</h1>
+      <p className="md:text-lg text-center text-gray-300">{desc}</p>
     </div>
   );
 }
