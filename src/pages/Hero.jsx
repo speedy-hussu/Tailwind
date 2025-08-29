89871e952f77f88f389612e8b655f19277271d8c
speedy-hussu <speedyhussu.work@gmail.com>
2025-08-30 00:30:13 +0530

    hero height

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -3,7 +3,7 @@ import webImg from "../assets/mobile.png";
 
 function Hero() {
   return (
-    <header className="w-full min-h-dvh flex flex-col-reverse items-center justify-center md:flex-row md:justify-around md:items-center md:pt-15 md:px-15 px-5 pt-15">
+    <header className="w-full h-180 flex flex-col-reverse items-center justify-center md:flex-row md:justify-around md:items-center md:pt-15 md:px-15 px-5 pt-15">
       <div className=" text-white  flex flex-col items-center break-words gap-5 mt-10 md:mr-10 md:items-start">
         <h1 className="uppercase  text-center lg:text-4xl font-extrabold text-3xl font-mono">
           play beyond limits.
